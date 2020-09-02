99865e6c90285c85c27b5b9ab7c80c657f121da3
Zelalem Gebeyehu Tessema <zelegt@gmail.com>
2020-09-03 00:28:19 +0300

    updated electron config

--- electron/config/config.js
+++ electron/config/config.js
@@ -5,14 +5,14 @@ const { app } = electron;
 module.exports = {
     development: {
         dialect: 'sqlite',
-        storage: path.join(app.getAppPath(), "cbhi_db.sqlite3"),
+        storage: path.join(app.getPath("userData"), "cbhi_db.sqlite3"),
         // Use a different storage type. Default: sequelize
         //app.getPath("userData")
         migrationStorage: "sequelize",
     },
     production: {
         dialect: 'sqlite',
-        storage: path.join(app.getAppPath(), "cbhi_db.sqlite3"),
+        storage: path.join(app.getPath("userData"), "cbhi_db.sqlite3"),
         // Use a different storage type. Default: sequelize
         migrationStorage: "sequelize",
     }
